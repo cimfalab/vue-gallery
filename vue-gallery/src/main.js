0ade319bb5156dd91fd96bedad63390ee6668e5f
Magdalena Dering <deringmagdalena@gmail.com>
2018-06-28 00:23:32 +0200

    added Slider component

--- vue-gallery/src/main.js
+++ vue-gallery/src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import BootstrapVue from "bootstrap-vue"
-import App from './App.vue'
+import Slider from "./App.vue"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
+import "./styles/global.css"
+
 
 Vue.use(BootstrapVue)
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app_gallery',
+    render: h => h(Slider)
 })
